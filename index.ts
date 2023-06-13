2c4bee13e6eabf4d4143094d25ccfa03ccd71996
LUFENGZHAN <xiaozhan2000423@163.com>
2023-06-13 16:53:12 +0800

    page

--- index.ts
+++ index.ts
@@ -7,6 +7,9 @@ export const MaxNum = (start:string,end:string):String => {
     if (!start && !end) {
         throw new Error('Two parameters cannot be empty')
     }
+    if (typeof(start) !== 'string' || typeof(end) !== 'string') {
+        throw new TypeError('Can only be a string')
+    }
     const len = Math.max(start.length, end.length)
     start = start.padStart(len,'0')
     end = end.padStart(len,'0')
