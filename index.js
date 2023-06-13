8e9e1fea23cc661b8b7cd4ac8cfa478c87bd80f3
LUFENGZHAN <xiaozhan2000423@163.com>
2023-06-13 16:56:57 +0800

    page

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ var MaxNum = function (start, end) {
     if (!start && !end) {
         throw new Error('Two parameters cannot be empty');
     }
+    if (typeof (start) !== 'string' || typeof (end) !== 'string') {
+        throw new TypeError('Can only be a string');
+    }
     var len = Math.max(start.length, end.length);
     start = start.padStart(len, '0');
     end = end.padStart(len, '0');
