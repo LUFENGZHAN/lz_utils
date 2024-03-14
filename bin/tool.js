e42023ab3ee48fbf747ff049a4141e3096cb6691
LUFENGZHAN <xiaozhan2000423@163.com>
2024-03-14 18:35:38 +0800

    init

--- bin/tool.js
+++
@@ -1,4 +0,0 @@
-export const tooltime = (month) => String(month).padStart(2, '0');
-export default {
-    tooltime
-};
--- types/tool.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const tooltime: (month: string | number) => String;
-declare const _default: {
-    tooltime: (month: string | number) => String;
-};
-export default _default;
